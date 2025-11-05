f703c1c74c35a142be3e96aad10913f8c8cd8b04
mparashkevov <m.parashkevov@gmail.com>
2025-11-05 09:13:27 +0200

    better visuals and game experience

--- day_12/guess_the_number.py
+++ day_12/guess_the_number.py
@@ -8,19 +8,28 @@ logo = """
                                ▐▌                                                
                                                                                  
                                                                                  """
+# Welcome message
 print(logo)
-print("Welcome to the Guessing Game!")
+print("\n" + "=" * 50 + "\n")
+print("Welcome to the Guessing Game!\n")
+print("=" * 50 + "\n")
+print("I'm thinking of a number between 1 and 100.")
 
+# Generate a random number between 1 and 100
 number_to_guess = random.randint(1, 100)
-print(f"Pssst, the correct answer is {number_to_guess}")
+#print(f"Pssst, the correct answer is {number_to_guess}")
 
+# Function to set difficulty level
 def set_difficulty():
-    level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    level = input("Choose a difficulty. Type 'easy' or 'hard': \n").lower()
+    print("\n" + "x" * 50 + "\n")
     if level == "easy":
         return 10
     else:
         return 5
 attempts_remaining = set_difficulty()
+
+# Function to check the player's guess
 def check_answer(guess, answer, attempts):
     if guess > answer:
         print("Too high.")
@@ -29,8 +38,12 @@ def check_answer(guess, answer, attempts):
         print("Too low.")
         return attempts - 1
     else:
-        print(f"You got it! The answer was {answer}.")
+        print("\n" + "$" * 50 + "\n")
+        print(f"!!! You got it! The answer was {answer} !!!")
+        print("\n" + "$" * 50 + "\n")
         return attempts
+
+# Game loop
 while attempts_remaining > 0:
     print(f"You have {attempts_remaining} attempts remaining to guess the number.")
     player_guess = int(input("Make a guess: "))
@@ -40,4 +53,4 @@ while attempts_remaining > 0:
     elif attempts_remaining == 0:
         print("You've run out of guesses, you lose.")
     else:
-        print("Guess again.")
+        print("Guess again." + "\n" + "x" * 50 + "\n")
