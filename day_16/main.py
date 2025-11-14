ce552afed7322656f47fbcabffb2d57584cc4559
mparashkevov <m.parashkevov@gmail.com>
2025-11-14 14:50:42 +0200

    new file:   day_16/main.py

---
+++ day_16/main.py
@@ -0,0 +1,14 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+picatchu_info_table = [
+    ["Name", "Picatchu"],
+    ["Type", "Electric"],
+    ["Level", "5"],
+    ["HP", "35"],
+    ["Attack", "55"],
+    ["Defense", "40"],
+    ["Speed", "90"],
+]
+
+print(table)
