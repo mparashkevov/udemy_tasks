01dcf177bc0717d137307197f488dbf241e26649
mparashkevov <m.parashkevov@gmail.com>
2025-12-09 16:30:04 +0200

    new file:   day_17/quiz-game-start/__pycache__/data.cpython-314.pyc 	new file:   day_17/quiz-game-start/__pycache__/question_model.cpython-314.pyc 	modified:   day_17/quiz-game-start/main.py 	modified:   day_17/quiz-game-start/quiz_brain.py

--- day_17/quiz-game-start/main.py
+++ day_17/quiz-game-start/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -11,4 +12,5 @@ for item in question_data:
 	question_bank.append(new_question)
 
 # question_bank now contains Question objects for each entry in data.py
-
+quiz = QuizBrain(question_bank)
+quiz.next_question()
---
+++ day_17/quiz-game-start/quiz_brain.py
@@ -0,0 +1,10 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+        self.score = 0
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        input(f"Q.{self.question_number}: {current_question.texrt} (True/False): ")
+
