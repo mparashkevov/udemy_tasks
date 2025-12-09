5c9c8dfce09f860deb96660fa9db179af233979c
mparashkevov <m.parashkevov@gmail.com>
2025-12-09 14:58:56 +0200

    modified:   .DS_Store 	new file:   day_17/.DS_Store 	new file:   day_17/quiz-game-start/data.py 	new file:   day_17/quiz-game-start/main.py 	new file:   day_17/quiz-game-start/question_model.py 	new file:   day_17/quiz-game-start/quiz_brain.py 	deleted:    day_17/uv_doc.md 	deleted:    day_17/uv_doc.mdx

---
+++ day_17/quiz-game-start/main.py
@@ -0,0 +1,14 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+# Create a Question object from each entry in question_data
+for item in question_data:
+	q_text = item['text']
+	q_answer = item['answer']
+	new_question = Question(q_text, q_answer)
+	question_bank.append(new_question)
+
+# question_bank now contains Question objects for each entry in data.py
+
