5b4a557055430eab5cdacaf17a1d4318466c8c78
mparashkevov <m.parashkevov@gmail.com>
2026-02-23 13:11:55 +0200

    feat: add ball reset position functionality and paddle miss detection

--- day_22_pong/main.py
+++ day_22_pong/main.py
@@ -33,5 +33,9 @@ while game_is_on:
     # Detect collision with paddle
     if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
         ball.bounce_x()
+    
+    # Detect when right or left paddle misses
+    if ball.xcor() > 380 or ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
