448d9248062e39f7a854a6eb58812f553604d9a7
mparashkevov <m.parashkevov@gmail.com>
2026-02-23 13:39:01 +0200

    feat: add Scoreboard class for score tracking and display

---
+++ day_22_pong/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.left_score = 0
+        self.right_score = 0
+        self.display_score()
+
+    def display_score(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(f"{self.left_score}", align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(f"{self.right_score}", align="center", font=("Courier", 80, "normal"))
+
+    def increase_left_score(self):
+        self.left_score += 1
+        self.display_score()
+
+    def increase_right_score(self):
+        self.right_score += 1
+        self.display_score()
