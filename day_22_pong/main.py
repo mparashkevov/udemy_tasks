f05623b867fe7ea0673a190853b03389629d778c
mparashkevov <m.parashkevov@gmail.com>
2026-02-23 12:57:40 +0200

    feat: implement Ball class with movement and collision methods

--- day_22_pong/main.py
+++ day_22_pong/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from ball import Ball
 from paddle import Paddle
 import time
 
@@ -17,9 +18,16 @@ screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
+ball = Ball()
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
+    ball.move()
     screen.update()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
 screen.exitonclick()
